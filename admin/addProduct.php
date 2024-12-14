e08fd98bf109cb2902c948d380341193b8135a7d
dsteiner19 <dsteiner@cord.edu>
2024-12-14 16:10:26 -0600

    php fun

---
+++ admin/addProduct.php
@@ -0,0 +1,38 @@
+<?php
+
+// Database connection
+$servername = "mysql.minglemug.knechtkode.com";
+$username = "minglemug"; 
+$password = "minglemug1204"; 
+$dbname = "productsInfo"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $productName = $_POST['productName'] ?? '';
+    $productPrice = $_POST['productPrice'] ?? 0.0;
+    $productQuantity = $_POST['productQuantity'] ?? 0;
+    $productType = $_POST['productCategory'] ?? '';
+    $timeOfDay = $_POST['timeOfDay'] ?? '';
+
+    if($productName && $productPrice && $productQuantity && $productType && $timeOFDay){
+
+        $stmt = $conn->prepare("INSERT INTO products (name, price, quantity, type, time_of_day Values(?, ?,?,?,?");
+        $STMT ->bind_param("sdisss",$productName,$productPrice,$productQuantity,$productType,$timeOfDay);
+
+        if($stmt->excute()){
+            echo"Product added successfully";
+            header("Location:productManagement.php");
+        }else{
+            echo "Error: ". $stmt->error;
+        }
+        $stmt->close();
+    }
+    else{
+        echo "Please fill in all fields";
+    }
+
+
+    }
+$conn->close();
+?>
