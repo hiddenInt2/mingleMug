6b4452a51305a2dda2e6d2624696db9a9f97a864
dsteiner19 <dsteiner@cord.edu>
2024-12-14 18:40:39 -0600

    e

---
+++ admin/deleteProduct.php
@@ -0,0 +1,12 @@
+<?php
+// Database connection
+$servername = "mysql.minglemug.knechtkode.com";
+$username = "minglemug"; 
+$password = "minglemug1204"; 
+$dbname = "productsInfo"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+?>
